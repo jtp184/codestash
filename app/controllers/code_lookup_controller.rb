e69d7382c8f13ef31931c695f82639dcd31646df
Justin Piotroski <justin.piotroski@gmail.com>
2017-04-24 11:16:30 -0700

    Added the upload button image for debugging, and fixed some of the views.

--- app/controllers/code_lookup_controller.rb
+++ app/controllers/code_lookup_controller.rb
@@ -1,3 +1,5 @@
+require 'zxing'
+
 class CodeLookupController < ApplicationController
 	def lookup
 	end
@@ -6,17 +8,24 @@ class CodeLookupController < ApplicationController
 		uploaded_io = params[:picture]
 		filename = "temp_barcode_#{Time.now().to_i}"
 		extension = "#{uploaded_io.original_filename.split('.')[1]}"
-		result = File.open(Rails.root.join('tmp', 'uploads', "#{filename}.#{extension}"), 'w+b') { |file| file.write(uploaded_io.read) }
 
-		resulting_key = (ZXing.decode File.open(result)).to_s
+		resolved_filename = Rails.root.join('tmp', 'uploads', "#{filename}.#{extension}")
+		result = File.open(resolved_filename, 'w+b') { |file| file.write(uploaded_io.read) }
 
-		if(uploaded_code.content.eql? "")
+		resulting_key = ZXing.decode(open(resolved_filename, 'rb') { |io| io.read })
+
+		if(resulting_key.nil? || resulting_key == '')
 			flash[:notice] = "Not a Clear enough Picture"
 			redirect_to action: "lookup"
 		elsif false 
 			# TODO: Add bot code
 		else
-			redirect_to controller: "code", action: "show", code_value: result
+			flash[:notice] = "Success! #{resulting_key}"
+			# redirect_to action: "lookup"
+			redirect_to action: "show", code_value: result
 		end
+	end
+
+	def show
 	end
 end
