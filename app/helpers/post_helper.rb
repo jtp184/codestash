fb69de41159f79d393d64e16d568446d0a4a16c3
Justin Piotroski <justin.piotroski@gmail.com>
2017-06-07 10:33:42 -0700

    More updates to the site layout. Added kind images and linked them properly

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,2 +1,8 @@
 module PostHelper
+	URL_REGEX = /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/
+
+	def self.get_url_base(url)
+		base = URL_REGEX.match(url)[3]
+		/(?:www\.)?(\w+\.\w+)/.match(base)[1]
+	end
 end
